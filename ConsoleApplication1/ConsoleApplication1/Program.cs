fce98325c8a6541baafd3bd8973a6678d1bde552
Harold Short <hshort@epicor.com>
2014-03-13 01:57:58 -0700

    Git is fun, again

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("Git is fun!");
             Console.WriteLine("Git is fun!");
+            Console.WriteLine("Git is fun!");
         }
     }
 }
